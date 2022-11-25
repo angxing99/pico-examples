581fe1906622e3485ffe27af7bcb655c8f204987
Ang Xing <quanxing99@gmail.com>
2022-11-25 15:11:01 +0800

    feat: mpu6050

--- blink/blink.c
+++ blink/blink.c
@@ -4,20 +4,35 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <stdio.h>
 #include "pico/stdlib.h"
 
-int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+int main()
+{
+    stdio_init_all();
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+
+    while (1)
+    {
+        gpio_put(25, 1);
+        printf("LED On !\n");
+        sleep_ms(1000);
+        gpio_put(25, 0);
+        sleep_ms(500);
     }
-#endif
+    // #ifndef PICO_DEFAULT_LED_PIN
+    // #warning blink example requires a board with a regular LED
+    // #else
+    //     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    //     gpio_init(LED_PIN);
+    //     gpio_set_dir(LED_PIN, GPIO_OUT);
+    //     while (true)
+    //     {
+    //         gpio_put(LED_PIN, 1);
+    //         sleep_ms(250);
+    //         gpio_put(LED_PIN, 0);
+    //         sleep_ms(250);
+    //     }
+    // #endif
 }
--- i2c/mpu6050_i2c/mpu6050_i2c.c
+++ i2c/mpu6050_i2c/mpu6050_i2c.c
@@ -1,8 +1,4 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
+
 
 #include <stdio.h>
 #include <string.h>
@@ -32,16 +28,29 @@
 
 // By default these devices  are on bus address 0x68
 static int addr = 0x68;
+int16_t rawAccX, rawAccY, rawAccZ, rawGyroX, rawGyroY, rawGyroZ;
+float gyroXoffset, gyroYoffset, gyroZoffset;
+float temp, accX, accY, accZ, gyroX, gyroY, gyroZ;
+float angleGyroX, angleGyroY, angleGyroZ, angleAccX, angleAccY, angleAccZ;
+float angleX, angleY, angleZ;
+float x = 0, y = 0, z = 0;
+float gyroXoffset, gyroYoffset, gyroZoffset;
+uint32_t timer, preInterval;
+float interval;
+float gyroCoef = 0.02;
+float accCoef = 0.98;
 
 #ifdef i2c_default
-static void mpu6050_reset() {
+static void mpu6050_reset()
+{
     // Two byte reset. First byte register, second byte data
     // There are a load more options to set up the device in different ways that could be added here
     uint8_t buf[] = {0x6B, 0x00};
     i2c_write_blocking(i2c_default, addr, buf, 2, false);
 }
 
-static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp)
+{
     // For this particular device, we send the device the register we want to read
     // first, then subsequently read from the device. The register is auto incrementing
     // so we don't need to keep sending the register we want, just the first.
@@ -53,7 +62,8 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
     i2c_read_blocking(i2c_default, addr, buffer, 6, false);
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
     }
 
@@ -61,26 +71,29 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     // The register is auto incrementing on each read
     val = 0x43;
     i2c_write_blocking(i2c_default, addr, &val, 1, true);
-    i2c_read_blocking(i2c_default, addr, buffer, 6, false);  // False - finished with bus
+    i2c_read_blocking(i2c_default, addr, buffer, 6, false); // False - finished with bus
 
-    for (int i = 0; i < 3; i++) {
-        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
+    for (int i = 0; i < 3; i++)
+    {
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+        ;
     }
 
     // Now temperature from reg 0x41 for 2 bytes
     // The register is auto incrementing on each read
     val = 0x41;
     i2c_write_blocking(i2c_default, addr, &val, 1, true);
-    i2c_read_blocking(i2c_default, addr, buffer, 2, false);  // False - finished with bus
+    i2c_read_blocking(i2c_default, addr, buffer, 2, false); // False - finished with bus
 
     *temp = buffer[0] << 8 | buffer[1];
 }
 #endif
 
-int main() {
+int main()
+{
     stdio_init_all();
 #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
-    #warning i2c/mpu6050_i2c example requires a board with I2C pins
+#warning i2c/mpu6050_i2c example requires a board with I2C pins
     puts("Default I2C pins were not defined");
 #else
     printf("Hello, MPU6050! Reading raw data from registers...\n");
@@ -98,18 +111,52 @@ int main() {
 
     int16_t acceleration[3], gyro[3], temp;
 
-    while (1) {
+    while (1)
+    {
         mpu6050_read_raw(acceleration, gyro, &temp);
+        int i;
+        for (i = 0; i < 1000; i++)
+        {
+            x += ((float)gyro[0]) / 65.5;
+            y += ((float)gyro[1]) / 65.5;
+            z += ((float)gyro[2]) / 65.5;
+        }
+        gyroXoffset = x / 1000;
+        gyroYoffset = y / 1000;
+        gyroZoffset = z / 1000;
+
+        accX = ((float)acceleration[0]) / 16384.0;
+        accY = ((float)acceleration[1]) / 16384.0;
+        accZ = ((float)acceleration[2]) / 16384.0;
+
+        gyroX = ((float)gyro[0]) / 65.5;
+        gyroY = ((float)gyro[1]) / 65.5;
+        gyroZ = ((float)gyro[2]) / 65.5;
+
+        gyroX -= gyroXoffset;
+        gyroY -= gyroYoffset;
+        gyroZ -= gyroZoffset;
+
+        angleGyroX += gyroX * interval;
+        angleGyroY += gyroY * interval;
+        angleGyroZ += gyroZ * interval;
+
+        interval = (timer - preInterval) * 0.001;
+
+        angleX = (gyroCoef * (angleX + gyroX * interval)) + (accCoef * angleAccX);
+        angleY = (gyroCoef * (angleY + gyroY * interval)) + (accCoef * angleAccY);
+        angleZ = angleGyroZ;
+        preInterval = timer;
 
         // These are the raw numbers from the chip, so will need tweaking to be really useful.
         // See the datasheet for more information
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
+        printf("Acc. X = %d, Y = %d, Z = %d\n", accX, accY, accZ);
         printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
         // Temperature is simple so use the datasheet calculation to get deg C.
         // Note this is chip temperature.
         printf("Temp. = %f\n", (temp / 340.0) + 36.53);
 
-        sleep_ms(100);
+        sleep_ms(1000);
     }
 
 #endif
